477d9a13ced34ac6b48d42a0bed9347f259ffbaf
wenx999 <wenx999@gmail.com>
2012-08-08 21:58:27 +0800

    case1

---
+++ case1.java
@@ -0,0 +1,5 @@
+   Long used = Long.valueOf(usedInfo.getAttributeAsLong("used"));
+   Long total = Long.valueOf(usedInfo.getAttributeAsLong("total"));
+   ips.add(new String(A + "." + B + "." + C + "." + D));
+
+
